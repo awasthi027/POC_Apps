5e9ce63ee4bf4510ce03d9fba75a428714601e48
Ashish Awasthi <awasthi027.ashish@gmail.com>
2023-10-23 13:44:49 +0530

    Added backgroud tag reading call in appdelegate

--- Apps/NFCPOC/NFCPOC/NFCPOCApp.swift
+++ Apps/NFCPOC/NFCPOC/NFCPOCApp.swift
@@ -9,23 +9,56 @@ import SwiftUI
 
 @main
 struct NFCPOCApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @State var rootView: RootScreen = .scanTag
+
     var body: some Scene {
         WindowGroup {
-            NavHandler {
-                ContentView()
+            switch self.rootView {
+            case .scanTag:
+                NavHandler {
+                    NFCScannerView()
+                        .environment(\.currentRootView, self.$rootView)
+                }
+            case .details(let nFCNDEFMessage):
+                NavHandler {
+                    NFCTagDetailsView(message: nFCNDEFMessage)
+                        .environment(\.currentRootView, self.$rootView)
+                }
+
             }
         }
     }
 }
 
-struct NavHandler<Content>: View where Content: View {
-    @ViewBuilder var content: () -> Content
+class AppDelegate: NSObject, UIApplicationDelegate {
 
-    var body: some View {
-        if #available(iOS 16, *) {
-            NavigationStack(root: content)
-        } else {
-            NavigationView(content: content)
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        if ProcessInfo.processInfo.arguments.contains("isRunningUITests") {
+            // Prepare application for UI tests.
+            print("UI Test are running....")
         }
+        return true
+    }
+    
+    /// This method will call when user press in control panel NFC tag
+    func application(_ application: UIApplication,
+                     continue userActivity: NSUserActivity,
+                     restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        guard userActivity.activityType == NSUserActivityTypeBrowsingWeb else {
+            return false
+        }
+
+        // Confirm that the NSUserActivity object contains a valid NDEF message.
+        let ndefMessage = userActivity.ndefMessagePayload
+        guard !ndefMessage.records.isEmpty,
+            ndefMessage.records[0].typeNameFormat != .empty else {
+                return false
+        }
+        
+        return true
     }
 }
+
